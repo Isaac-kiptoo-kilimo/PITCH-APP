e5b7f88b6f304f789a33e9f787e6aa12d1361eb6
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-08 19:12:02 +0300

    feature:added blueprint

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+# from distutils.command.config import config
+# from ensurepip import bootstrap
+# from ssl import Options
+# from click import option
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap=Bootstrap()
+
+def create_app(config_name):
+    app=Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # initializing the flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ config.py
@@ -0,0 +1,18 @@
+
+import os
+
+from flask import Config
+
+class Confif:
+    SQLAlchemy_DATABASE_URI=os.getenv('DATABASE_URL')
+    DEBUG=os.getenv('DEBUG')
+
+class ProdConfig(Config):
+    pass
+class DevConfig(Config):
+    pass
+
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig
+}
