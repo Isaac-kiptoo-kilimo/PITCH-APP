ee63498e1cd3459d570115580dee004afa7d5ed0
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-09 07:34:23 +0300

    feature:retrieving data

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for, flash
 from . import main
 from .. import db, login_manager
 from ..models import User, Pitch,Comment, Category
@@ -33,13 +33,55 @@ def index():
 
 @main.route('/pitches/categories/<string:category_id>')
 def category_view(category_id):
-  return render_template('pages/category.html')
-
+  category = Category.query.filter_by(id=category_id).first()
+  categories = Category.query.order_by(Category.id.asc()).all()
+  print("category", category)
+  if category:
+    pitches = Pitch.query.filter_by(category_id=category.id).all()
+    new_pitches = make_pitches(pitches)
+    return render_template('pages/category.html',categories=categories, category=category, pitches=new_pitches)
+  else:
+    flash('Category not found', 'warning')
+    return redirect(url_for('main.index'))
 
 @main.route('/pitches/view/<string:pitch_id>/')
+@login_required
 def pitch_view(pitch_id):
+  pitch = Pitch.query.filter_by(id=pitch_id).first()
+  if pitch:
+    comments = Comment.query.filter_by(pitch_id=pitch.id).all()
+    new_comments = []
+    for comment in comments:
+      user = User.query.filter_by(id=comment.user_id).first()
+      new_comments.append({
+        'id': comment.id,
+        'content': comment.content,
+        'user': user,
+        'created_at': comment.created_at
+      })
+    user = User.query.filter_by(id=pitch.user_id).first()
   
-  return render_template('pages/pitchview.html')
+    return render_template('pages/pitchview.html',pitch=pitch, comments=new_comments, user=user)
+  else:
+    flash('Pitch not found', 'warning')
+    return redirect(url_for('main.index'))
+
+@main.route('/pitches/view/<string:pitch_id>/comments/add/', methods=['GET', 'POST'])
+@login_required
+def add_comment(pitch_id):
+  pitch = Pitch.query.filter_by(id=pitch_id).first()
+  if request.method == 'POST':
+    content = request.form['comment']
+    if pitch:
+      comment = Comment(pitch_id=pitch.id, user_id=current_user.id, content=content)
+      db.session.add(comment)
+      db.session.commit()
+      flash('Comment added', 'success')
+      return redirect(url_for('main.pitch_view', pitch_id=pitch.id))
+    else:
+      flash('Pitch not found', 'warning')
+      return redirect(url_for('main.index'))
+  return redirect(url_for('main.pitch_view', pitch_id=pitch.id))
 
 
 @main.route('/auth/login', methods=['GET','POST'])
